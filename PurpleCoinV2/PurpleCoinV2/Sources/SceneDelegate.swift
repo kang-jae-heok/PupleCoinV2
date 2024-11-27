b862bf000b0b58a5569c31d2f2acf7ccf196e777
kang-jae-heok <kjhabc8707@naver.com>
2024-11-27 21:49:18 +1100

    refactor: swift ui기반 -> UIkit 기반 리펙터링

---
+++ PurpleCoinV2/PurpleCoinV2/Sources/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  Core
+//
+//  Created by 강재혁 on 11/27/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
+        let mainViewController = ViewController() // 맨 처음 보여줄 ViewController
+
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
