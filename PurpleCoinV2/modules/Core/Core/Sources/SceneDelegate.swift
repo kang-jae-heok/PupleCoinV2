9b81c6aa809081519e98b877287a67dc78aa6e62
kang-jae-heok <kjhabc8707@naver.com>
2024-11-27 21:49:18 +1100

    refactor: swift ui기반 -> UIkit 기반 리펙터링

---
+++ PurpleCoinV2/modules/Core/Core/Sources/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  Core
+//
+//  Created by 강재혁 on 11/27/24.
+//
+
+import Foundation
---
+++ PurpleCoinV2/modules/Core/Core/Sources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Core
+//
+//  Created by 강재혁 on 11/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
